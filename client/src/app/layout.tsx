6fe1fd1c47bb569320d14a140c19b9c8cf843958
boro-dach <140444699+boro-dach@users.noreply.github.com>
2024-07-14 11:51:27 +0200

    added auth

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Unbounded } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import SessionWrapper from '@/components/SessionWrapper'
+import Header from '../components/Header';
+import SessionWrapper from '../components/SessionWrapper'
 
 const unbounded = Unbounded({ subsets: ["latin"] });
 
