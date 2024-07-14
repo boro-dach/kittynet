c53f889fb25ebe57c4a24a30899deddc6ef8a948
boro-dach <140444699+boro-dach@users.noreply.github.com>
2024-07-14 15:27:27 +0200

    sign in / sign out button change

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,18 @@
 "use client"
 
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession} from "next-auth/react";
 import Link from "next/link";
 
 export default function Header() {
+  const { data: session } = useSession();
   return (
     <header className=" h-16 m-4 bg-zinc-900 rounded-xl flex justify-between items-center">
         <Link href='/'><h1 className=" ml-8 font-bold text-2xl ">kittynet</h1></Link>
-        <button onClick={() => signIn()} className=" mr-8 px-4 py-2 bg-zinc-50 text-black rounded-xl hover:brightness-50 transition-all duration-200">войти</button>
+        {session ? (
+          <button onClick={() => signOut()} className=" mr-8 px-4 py-2 bg-zinc-50 text-black rounded-xl hover:brightness-50 transition-all duration-200">выйти(</button>
+        ) : (
+          <button onClick={() => signIn()} className=" mr-8 px-4 py-2 bg-zinc-50 text-black rounded-xl hover:brightness-50 transition-all duration-200">войти</button>
+        )}
     </header>
   ) 
 }
